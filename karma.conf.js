65e19f18ad7138c10f64ff4de98755554de6d07f
Lindsay Pallickal <pallickal@users.noreply.github.com>
2016-08-11 01:01:58 -0400

    chore: Integrate sinon & enzyme with karma for testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,59 @@
 var webpack = require('webpack');
 var path = require('path');
+var argv = require('yargs').argv;
 
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 module.exports = function (config) {
   config.set({
-    browsers: [ 'Chrome' ], //run in Chrome
-    singleRun: true, //just run once by default
-    frameworks: [ 'mocha' ], //use the mocha test framework
+    browsers: [ 'Chrome' ],
+    singleRun: !argv.watch,
+    frameworks: [ 'mocha', 'chai', 'sinon' ],
     files: [
-      'tests.webpack.js' //just load this file
+      'tests.webpack.js'
     ],
     preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
-    reporters: [ 'dots' ], //report results in this format
-    webpack: { //kind of a copy of your webpack config
-      devtool: 'inline-source-map', //just do inline source maps instead of the default
+    reporters: [ 'spec' ],
+    webpack: {
+      devtool: 'inline-source-map',
       resolve: {
         root: path.resolve(__dirname, APP_DIR),
         extensions: ['', '.js', '.jsx'],
+        alias: {
+         'sinon': 'sinon/pkg/sinon'
+        }
       },
       module: {
+        noParse: [
+          /node_modules\/sinon\//
+        ],
         loaders: [
           { test: /\.jsx?$/,
             include: APP_DIR,
             loader: 'babel-loader' }
         ]
+      },
+      // required for enzyme to work properly
+      externals: {
+        'cheerio': 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
       }
     },
     webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    }
+      noInfo: true
+    },
+    plugins: [
+      'karma-mocha',
+      'karma-chai',
+      'karma-sinon',
+      'karma-chrome-launcher',
+      'karma-webpack',
+      'karma-spec-reporter',
+      'karma-sourcemap-loader'
+    ]
   });
 };
